a4586b37a85a56891a361bd691ece476302e1363
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-28 13:00:46 +0200

    Added microservice Vote-Service

---
+++ microservice-Vote-Service/db/Vote-Service_create_tables.sql
@@ -0,0 +1,21 @@
+--
+-- Database Schema:  test 
+-- Automatically generated sql script for the service Vote-Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table user.
+--
+CREATE TABLE test.user (
+  userId int ,
+CONSTRAINT userId_PK PRIMARY KEY (userId)
+);
+--
+-- Table structure for table numbers.
+--
+CREATE TABLE test.numbers (
+  id int NOT NULL AUTO_INCREMENT ,
+  number int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
